427670df14073fef6e8bd1fc6de00e4eecacfee1
Will Thomas <30wthomas@gmail.com>
2022-08-12 09:46:39 -0500

    More general typeclass constructability

--- TypeClasses.v
+++ TypeClasses.v
@@ -1,4 +1,7 @@
 Require Import Ltacs.
+Require Import String.
+
+Set Typeclasses Debug.
 
 Class Dec (P : Prop) := { dec : P + ~ P }.
 
@@ -10,35 +13,88 @@ Proof.
   destruct H; destruct dec0; eauto.
 Qed.
 
-Class DecEq {A : Type} (x1 x2 : A) `(Dec (x1 = x2)) := 
+Class DecEq (A : Type) := 
 {
-  decEq : { x1 = x2 } + {x1 <> x2}
+  decEq : forall x1 x2 : A, { x1 = x2 } + {x1 <> x2}
 }.
 
 Lemma nat_eq_dec' : forall n1 n2 : nat,
     {n1 = n2} + {n1 <> n2}.
 Proof with eauto.
-  induction n1; destruct n2...
+  ind n1; destruct n2...
   destruct (IHn1 n2)...
 Qed.
+
+(** Setting up some baseline Decidable props related to equality
+    - This should allow for the other classes to be easily built
+*)
 
 #[global]
 Instance nat_eq_dec (n1 n2 : nat) : Dec (n1 = n2).
 destruct (nat_eq_dec' n1 n2); constructor; eauto.
 Defined.
 
-(* 
-  Any type that has a decision procedure for equality -> equality is decidable
-*)
 #[global]
-Instance dec_impl_deceq {A : Type} (a1 a2 : A) `{D : Dec (a1 = a2)} : DecEq a1 a2 D.
-constructor. inv D. inv dec0; eauto. Defined.
+Instance decEq_nat : DecEq nat.
+constructor. ind x1; destruct x2; eauto.
+specialize IHx1 with x2.
+destruct IHx1; eauto. Defined.
 
 #[global]
-Instance decEq_nat (n1 n2 : nat) `{d : Dec (n1 = n2)} : DecEq n1 n2 d.
-apply (@dec_impl_deceq nat n1 n2 d). Defined.
+Instance decEq_string : DecEq string.
+constructor. apply string_dec. Defined.
 
 #[global]
-Instance decEq_string (s1 s2 : string) `(d : Dec (s1 = s2)) : 
+Instance decEq_list (A : Type) `{DA : DecEq A} : DecEq (list A).
+constructor. 
+ind x1; dest x2; eauto; try (right; qcon; fail). 
+(* Checking for list head equality *)
+dest DA. dest (decEq0 a a0);
+specialize IHx1 with x2; 
+dest IHx1; subst; eauto; try (right; qcon; congruence).
+Defined.
 
-Class Ord `(E : EqDec A) := { le : A -> A -> bool }.
+#[global]
+Instance decEq_pair (A B : Type) `{DA : DecEq A} `{DB : DecEq B} : DecEq (A * B).
+constructor.
+ind x1; dest x2; eauto; try (right; qcon; fail).
+dest DA; dest DB; dest (decEq0 a a0); dest (decEq1 b b0); 
+subst; eauto; try (right; qcon; congruence).
+Defined.
+
+Class EqClass (A : Type) `{DE : DecEq A} :=
+{
+  eqb : A -> A -> bool ;
+  eqb_leibniz   : forall x y, eqb x y = true <-> x = y  ;
+  neqb_leibniz  : forall x y, eqb x y = false <-> x <> y;
+}.
+
+Definition gen_deceq_eqb {A : Type} `{DE : DecEq A} (a1 a2 : A) : bool :=
+  match (decEq a1 a2) with
+  | left e => true
+  | right e => false
+  end.
+
+Lemma gen_eqb_impl_eqb_leibniz : forall {A : Type} `{Eq : DecEq A} (x y : A), 
+  gen_deceq_eqb x y = true <-> x = y.
+Proof.
+  unfold gen_deceq_eqb.
+  intros.
+  destruct (decEq x y); split; eauto; try qcon.
+Defined.
+
+Lemma gen_eqb_impl_neqb_leibniz : forall {A : Type} `{Eq : DecEq A} (x y : A),
+  gen_deceq_eqb x y = false <-> x <> y.
+Proof.
+  unfold gen_deceq_eqb.
+  intros.
+  destruct (decEq x y); split; eauto; try qcon.
+Defined.
+
+#[global]
+Instance deceq_impl_eqb (A : Type) `{DE : DecEq A} : EqClass A :=
+{
+  eqb := gen_deceq_eqb ;
+  eqb_leibniz := gen_eqb_impl_eqb_leibniz ;
+  neqb_leibniz := gen_eqb_impl_neqb_leibniz
+}.
